18d271c7a2d388e649a922672817b9dda9241f5f
knada <nanaayimda@gmail.com>
2021-03-16 15:35:30 +0000

    added head to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Nav from '../Nav/Nav'
 import styles from './Layout.module.css'
 
 const Layout = ({ children }) => {
     return (
         <div className={styles.container}>
+            <Head>
+                <title>Innospace</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Nav />
             {children}
         </div>
