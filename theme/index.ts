82b8673b5dfe16229326ad4181059c79945bc51b
knada <nanaayimda@gmail.com>
2021-03-17 14:54:40 +0000

    Added main site theme

---
+++ theme/index.ts
@@ -0,0 +1,25 @@
+const colors = {
+    PRIMARY: '#1C2E36',
+    SECONDARY: '#25BCCA',
+    GREY: '#D6E1E4',
+    LIGHTGREY: 'E6EDEF',
+    DARKGREY: 'A4B3B7',
+    headingColor: 'hsl(0, 0%, 15%)',
+    bodyColor: 'hsl(0, 0%, 35%)',
+    innospaceGreen: 'hsl(165, 90%, 37%)',
+    facebook: 'hsl(221, 44%, 41%)',
+    twitter: 'hsl(203, 89%, 53%)',
+    instagram: 'hsl(326, 57%, 48%)',
+}
+
+const typography = {
+    primaryFont: '"Poppins", Arial, Helvetica, sans-serif',
+    altFont: '"Muli", Arial, Helvetica, sans-serif',
+}
+
+const theme = {
+    colors,
+    typography,
+}
+
+export default theme
