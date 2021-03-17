0f1ecae810b7f0537d8580827d16e7cec1bf6207
knada <nanaayimda@gmail.com>
2021-03-17 15:30:33 +0000

    fixed import path

--- components/MainNav/MainNav.tsx
+++ components/MainNav/MainNav.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import styled, { keyframes } from 'styled-components'
-import InnoSpaceLogo from '../SvgComponents/InnoSpaceLogo/InnoSpaceLogo'
+import InnoSpaceLogo from '../SvgComponents/InnoSpaceLogo'
 
 type DisplayType = 'default' | 'opened' | 'closed'
 
