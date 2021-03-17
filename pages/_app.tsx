1e1f1a49341ddfdd083fcc528b3188570e5f270e
knada <nanaayimda@gmail.com>
2021-03-17 14:54:17 +0000

    Setup styled components for project with global styles and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import theme from '../theme'
+
+const GlobalStyles = createGlobalStyle`
+    *, *::before, *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        font-family: 'Poppins', Arial, Helvetica, sans-serif;
+        font-style: normal;
+        text-rendering: optimizeLegibility;
+        scroll-behavior: smooth;
+    }
+`
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <GlobalStyles />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default MyApp
