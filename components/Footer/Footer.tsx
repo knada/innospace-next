822a9a4cbf92d9c796b6520856cd9ed515536ce7
knada <nanaayimda@gmail.com>
2021-03-17 07:12:47 +0000

    Added Footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <section className={styles.section}>
+            <div className={styles.container}></div>
+        </section>
+    )
+}
+
+export default Footer
