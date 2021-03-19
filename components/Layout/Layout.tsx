446976ee0e62f9c1356855f68d3008cbebebe77c
knada <nanaayimda@gmail.com>
2021-03-19 00:45:59 +0000

    Removed css modules and switched to styled components

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
+import styled from 'styled-components'
+import Footer from '../Footer/Footer'
 import MainNav from '../MainNav/MainNav'
-import styles from './Layout.module.css'
+
+const Main = styled.main``
 
 const Layout = ({ children }) => {
     return (
-        <div className={styles.container}>
+        <>
             <Head>
                 <title>Innospace</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <MainNav />
-            {children}
-        </div>
+            <Main>{children}</Main>
+            <Footer />
+        </>
     )
 }
 
