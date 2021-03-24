5289cd19ee87b8e5ecc181e98d7aa78f6f0175f0
knada <nanaayimda@gmail.com>
2021-03-24 14:26:55 +0000

    Styled homepage and added airtable data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,41 @@
-import { MainHeading } from '../components/Headings/Headings'
+import styled from 'styled-components'
+import airtable from '../Airtable'
+import Hero from '../components/Hero/Hero'
 import Layout from '../components/Layout/Layout'
 
-export default function Home() {
+const HeaderWithBg = styled.div`
+    width: 100%;
+    height: 780px;
+    background-image: linear-gradient(
+        hsla(0, 0%, 10%, 0.9),
+        hsla(0, 0%, 10%, 0.9)
+    );
+    display: flex;
+`
+
+const tableName = 'Hero'
+
+export async function getStaticProps() {
+    const data = await airtable
+        .base(process.env.AIRTABLE_BASE_ID)(tableName)
+        .find('recHu3lxl0R1xFLeR')
+    const main = await data.get('Main')
+    const sub = await data.get('Sub')
+
+    return {
+        props: {
+            main,
+            sub,
+        },
+    }
+}
+
+export default function Home({ main, sub }) {
     return (
         <Layout>
-            <MainHeading>Home Page</MainHeading>
+            <HeaderWithBg>
+                <Hero main={main} sub={sub} />
+            </HeaderWithBg>
         </Layout>
     )
 }
