34e56af5a9ddebc22f730e4ac04b5b3fe4a9a147
knada <nanaayimda@gmail.com>
2021-04-01 23:03:55 +0000

    Added page template for non-homepage pages

---
+++ components/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import { MainHeading } from '../Headings/Headings'
+import Layout from '../Layout/Layout'
+
+const PageHero = styled.div`
+    width: 100%;
+    height: 400px;
+    background-color: hsla(151, 50%, 60%, 1);
+    background-image: linear-gradient(
+        hsla(250, 20%, 10%, 0.3),
+        hsla(250, 20%, 10%, 0.3)
+    );
+    background-position: center;
+    background-repeat: repeat;
+    background-size: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width: 960px) {
+        height: 300px;
+    }
+    @media screen and (max-width: 600px) {
+        height: 200px;
+    }
+`
+
+const PageHeroHeading = styled(MainHeading)`
+    color: white;
+`
+
+const PageTemplate = ({ children, heading }) => {
+    return (
+        <Layout>
+            <PageHero>
+                <PageHeroHeading>{heading}</PageHeroHeading>
+            </PageHero>
+            {children}
+        </Layout>
+    )
+}
+
+export default PageTemplate
