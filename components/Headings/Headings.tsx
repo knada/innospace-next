c422c853353bbd3df1795824bd6d9a9f05faf6d4
knada <nanaayimda@gmail.com>
2021-05-03 17:21:07 +0000

    Removed css module styles and replaced with styled components using site styles

--- components/Headings/Headings.tsx
+++ components/Headings/Headings.tsx
@@ -1,13 +1,28 @@
-import styles from './Headings.module.css'
+import styled from 'styled-components'
 
-export const MainHeading: React.FC = ({ children }) => (
-    <h1 className={styles.main}>{children}</h1>
-)
+export const MainHeading = styled.h1`
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-size: 54px;
+    font-weight: 700;
+    line-height: 1.5;
+    text-align: center;
+    @media screen and (max-width: 700px) {
+        font-size: 42px;
+    }
+`
 
-export const SectionHeading: React.FC = ({ children }) => (
-    <h2 className={styles.section}>{children}</h2>
-)
+export const SectionHeading = styled.h2`
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-size: 42px;
+    font-weight: 600;
+    line-height: 1.25;
+    text-align: center;
+    margin: 2rem;
+`
 
-export const SubHeading: React.FC = ({ children }) => (
-    <h3 className={styles.subheading}>{children}</h3>
-)
+export const SubHeading = styled.h3`
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 1.25;
+    text-align: center;
+`
