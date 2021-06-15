afff933de2bcf8eb2f97fbb463e7a6efdfade0f5
knada <nanaayimda@gmail.com>
2021-06-15 17:38:17 +0000

    configured about page to request notes data from airtable and use the notes returned on the page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,32 @@
-import { MainHeading } from '../components/Headings/Headings'
-import Layout from '../components/Layout/Layout'
+import airtable from '../Airtable'
+import AboutContent from '../components/PageContent/AboutContent/AboutContent'
+import PageTemplate from '../components/PageTemplate/PageTemplate'
 
-export default function About() {
+const tableName = 'About'
+
+const getNotes = () => {
+    return new Promise((resolve, reject) => {
+        airtable(tableName)
+            .select()
+            .firstPage((err, records) => {
+                if (err) return reject(err)
+                return resolve(records.map(record => record.get('Notes')))
+            })
+    })
+}
+
+export const getStaticProps = async () => {
+    return {
+        props: {
+            notes: await getNotes(),
+        },
+    }
+}
+
+export default function About({ notes }) {
     return (
-        <Layout>
-            <MainHeading>About Us</MainHeading>
-        </Layout>
+        <PageTemplate heading="About Us">
+            <AboutContent notes={notes} />
+        </PageTemplate>
     )
 }
