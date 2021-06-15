2bf28b1bb45af6cb66e2d0e591a47c750cefed81
knada <nanaayimda@gmail.com>
2021-06-15 17:41:31 +0000

    configure spaces page to get spaces data from airtable and display with spaces content component

--- pages/spaces.tsx
+++ pages/spaces.tsx
@@ -1,10 +1,42 @@
-import { MainHeading } from '../components/Headings/Headings'
-import Layout from '../components/Layout/Layout'
+import airtable from '../Airtable'
+import SpacesContent from '../components/PageContent/SpacesContent/SpacesContent'
+import PageTemplate from '../components/PageTemplate/PageTemplate'
 
-export default function Spaces() {
+const tableName = 'Spaces'
+
+const getSpaces = () => {
+    return new Promise((resolve, reject) => {
+        airtable(tableName)
+            .select()
+            .firstPage((err, records) => {
+                if (err) return reject(err)
+                return resolve(
+                    records.map(record => {
+                        return {
+                            id: record.id,
+                            space: record.get('Space'),
+                            description: record.get('Description'),
+                            image: record.get('Attachments')[0],
+                        }
+                    })
+                )
+            })
+    })
+}
+
+export const getStaticProps = async () => {
+    console.log(await getSpaces())
+    return {
+        props: {
+            spaces: await getSpaces(),
+        },
+    }
+}
+
+export default function Spaces({ spaces }) {
     return (
-        <Layout>
-            <MainHeading>Our Spaces</MainHeading>
-        </Layout>
+        <PageTemplate heading="Our Spaces">
+            <SpacesContent spaces={spaces} />
+        </PageTemplate>
     )
 }
