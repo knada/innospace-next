4ddd0ea74c120d1ff11fbf67915bce12fbb60390
knada <nanaayimda@gmail.com>
2021-07-21 18:33:08 +0000

    Configured membership page to fetch membership data from airtable for memberships component

--- pages/memberships.tsx
+++ pages/memberships.tsx
@@ -1,10 +1,66 @@
+import airtable from '../Airtable'
 import MembershipsContent from '../components/PageContent/MembershipsContent/MembershipsContent'
 import PageTemplate from '../components/PageTemplate/PageTemplate'
 
-export default function Memberships() {
+const memberships = {
+    openDesk: 'Open Desk',
+    openMemberships: 'Open Memberships',
+    privateOffice: 'Private Office',
+    meeting: 'Meeting Room',
+    multipurpose: 'Multipurpose Centre',
+}
+
+const getTableData = tableName => {
+    return new Promise((resolve, reject) => {
+        airtable(tableName)
+            .select()
+            .firstPage((err, records) => {
+                if (err) return reject(err)
+                return resolve(
+                    records.map(record => {
+                        return {
+                            id: record.id,
+                            fields: record.fields,
+                        }
+                    })
+                )
+            })
+    })
+}
+
+export const getStaticProps = async () => {
+    const openDesk = await getTableData(memberships.openDesk)
+    const openMemberships = await getTableData(memberships.openMemberships)
+    const privateOffice = await getTableData(memberships.privateOffice)
+    const meeting = await getTableData(memberships.meeting)
+    const multipurpose = await getTableData(memberships.multipurpose)
+    return {
+        props: {
+            openDesk,
+            openMemberships,
+            privateOffice,
+            meeting,
+            multipurpose,
+        },
+    }
+}
+
+export default function Memberships({
+    openDesk,
+    openMemberships,
+    privateOffice,
+    meeting,
+    multipurpose,
+}) {
     return (
         <PageTemplate heading="Our Memberships">
-            <MembershipsContent />
+            <MembershipsContent
+                openDesk={openDesk}
+                openMemberships={openMemberships}
+                privateOffice={privateOffice}
+                meeting={meeting}
+                multipurpose={multipurpose}
+            />
         </PageTemplate>
     )
 }
