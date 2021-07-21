3051ff01e0de6e3a0e90cd8ff9e8f37c6da6e580
knada <nanaayimda@gmail.com>
2021-07-21 18:33:48 +0000

    Created tables to display membership content fetched from airtable

--- components/PageContent/MembershipsContent/MembershipsContent.tsx
+++ components/PageContent/MembershipsContent/MembershipsContent.tsx
@@ -1,5 +1,23 @@
+import { FC } from 'react'
 import styled from 'styled-components'
 import InnerContainer from '../../InnerContainer/InnerContainer'
+
+type membershipType = {
+    id: string
+    fields: {
+        Categories: string
+        Cost: number
+        Services: string
+    }
+}
+
+type MembershipsContentProps = {
+    openDesk?: membershipType[]
+    openMemberships?: membershipType[]
+    privateOffice?: membershipType[]
+    meeting?: membershipType[]
+    multipurpose?: membershipType[]
+}
 
 const MembershipSection = styled.section``
 
@@ -10,7 +28,7 @@ const MembershipDiv = styled.div`
 `
 
 const MembershipTitle = styled.h3`
-    font-family: 'Muli', Arial, Helvetica, sans-serif;
+    font-family: 'Mulish', Arial, Helvetica, sans-serif;
     margin-top: 0.5rem;
 `
 
@@ -53,7 +71,13 @@ const TableData = styled.td`
     }
 `
 
-const MembershipsContent = () => {
+const MembershipsContent: FC<MembershipsContentProps> = ({
+    openDesk,
+    openMemberships,
+    privateOffice,
+    meeting,
+    multipurpose,
+}) => {
     return (
         <MembershipSection>
             <MembershipContainer>
@@ -69,6 +93,172 @@ const MembershipsContent = () => {
                         tea/coffee, private lockable and furnished offices and
                         signage display.{' '}
                     </MembershipBodyText>
+                </MembershipDiv>
+                <MembershipDiv>
+                    <MembershipTitle>
+                        Open Desk(Day, Weekly & Monthly Pass)
+                    </MembershipTitle>
+                    <MembershipTable>
+                        <MembershipTableHead>
+                            <TableRow>
+                                <TableHeader>Desk Category</TableHeader>
+                                <TableHeader>Cost</TableHeader>
+                                <TableHeader>Services</TableHeader>
+                            </TableRow>
+                        </MembershipTableHead>
+                        <TableBody>
+                            {openDesk.map(node => (
+                                <TableRow key={node.id}>
+                                    <TableData>
+                                        {node.fields.Categories}
+                                    </TableData>
+                                    <TableData>
+                                        GH₵{node.fields.Cost}/month
+                                    </TableData>
+                                    <TableData>
+                                        {node.fields.Services}
+                                    </TableData>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </MembershipTable>
+                </MembershipDiv>
+                <MembershipDiv>
+                    <MembershipTitle>
+                        Co working Annual Pass: Membership subscription (minimum
+                        3 months)
+                    </MembershipTitle>
+                    <MembershipTable>
+                        <MembershipTableHead>
+                            <TableRow>
+                                <TableHeader>Membership</TableHeader>
+                                <TableHeader>Cost</TableHeader>
+                                <TableHeader>Services</TableHeader>
+                            </TableRow>
+                        </MembershipTableHead>
+                        <TableBody>
+                            {openMemberships.map(node => (
+                                <TableRow key={node.id}>
+                                    <TableData>
+                                        {node.fields.Categories}
+                                    </TableData>
+                                    <TableData>
+                                        GH₵{node.fields.Cost}/month
+                                    </TableData>
+                                    <TableData>
+                                        {node.fields.Services}
+                                    </TableData>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </MembershipTable>
+                </MembershipDiv>
+                <MembershipDiv>
+                    <MembershipTitle>Private Offices (5 sqft)</MembershipTitle>
+                    <MembershipTable>
+                        <MembershipTableHead>
+                            <TableRow>
+                                <TableHeader>Category</TableHeader>
+                                <TableHeader>Cost</TableHeader>
+                                <TableHeader>Services</TableHeader>
+                            </TableRow>
+                        </MembershipTableHead>
+                        <TableBody>
+                            {privateOffice.map(node => (
+                                <TableRow key={node.id}>
+                                    <TableData>
+                                        {node.fields.Categories}
+                                    </TableData>
+                                    <TableData>
+                                        GH₵ {node.fields.Cost}/month (any
+                                        additional head will attract 100 per
+                                        month)
+                                    </TableData>
+                                    <TableData>
+                                        {node.fields.Services}
+                                    </TableData>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </MembershipTable>
+                    <MembershipBodyText>
+                        Enjoy discount of GHC 1000 at six months plus.
+                    </MembershipBodyText>
+                </MembershipDiv>
+                <MembershipDiv>
+                    <MembershipTitle>Short Meeting Room</MembershipTitle>
+                    <MembershipTable>
+                        <MembershipTableHead>
+                            <TableRow>
+                                <TableHeader>Category</TableHeader>
+                                <TableHeader>Cost</TableHeader>
+                                <TableHeader>Services</TableHeader>
+                            </TableRow>
+                        </MembershipTableHead>
+                        <TableBody>
+                            {meeting.map((node, index) => (
+                                <TableRow key={node.id}>
+                                    <TableData>
+                                        {node.fields.Categories}
+                                    </TableData>
+                                    <TableData>
+                                        {index === 0
+                                            ? `GH₵ ${node.fields.Cost}/4hrs`
+                                            : `GH₵ ${node.fields.Cost}`}
+                                    </TableData>
+                                    <TableData>
+                                        {node.fields.Services}
+                                    </TableData>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </MembershipTable>
+                    <MembershipTitle>Payment conditions</MembershipTitle>
+                    <MembershipBodyText>
+                        For short-term occupancy(minimum of 3 months, maximum of
+                        6 months), fees are paid upfront (pay per use). For
+                        long-term occupancy(beyond 6 months), a tenancy contract
+                        shall be issued and a payment schedule shall be agreed
+                        upon.
+                    </MembershipBodyText>
+                </MembershipDiv>
+                <MembershipDiv>
+                    <MembershipTitle>Multipurpose centre</MembershipTitle>
+                    <MembershipBodyText>
+                        Our multipurpose facility is ideal for meetings,
+                        conferences, staff training, video presentation and
+                        social events. It is equipped with state-of-the-art
+                        audio visual equipment for your purpose and can
+                        accommodate 50 people at a sitting.
+                    </MembershipBodyText>
+                    <MembershipTable>
+                        <MembershipTableHead>
+                            <TableRow>
+                                <TableHeader>Category</TableHeader>
+                                <TableHeader>Cost</TableHeader>
+                                <TableHeader>Services</TableHeader>
+                            </TableRow>
+                        </MembershipTableHead>
+                        <TableBody>
+                            {multipurpose.map((node, index) => (
+                                <TableRow key={node.id}>
+                                    <TableData>
+                                        {node.fields.Categories}
+                                    </TableData>
+                                    <TableData>
+                                        {index === 0
+                                            ? `GH₵ ${node.fields.Cost}/2hrs`
+                                            : index === 1
+                                            ? `GH₵ ${node.fields.Cost}/4hrs`
+                                            : `GH₵ ${node.fields.Cost}/daily`}
+                                    </TableData>
+                                    <TableData>
+                                        {node.fields.Services}
+                                    </TableData>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </MembershipTable>
                 </MembershipDiv>
             </MembershipContainer>
         </MembershipSection>
