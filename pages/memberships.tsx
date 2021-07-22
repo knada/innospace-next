b72c04f1f185beeeec23bb668ec55e067ec2693d
knada <nanaayimda@gmail.com>
2021-07-22 12:53:30 +0000

    replaced local function with utility function getTableData and removed comments

--- pages/memberships.tsx
+++ pages/memberships.tsx
@@ -10,24 +10,6 @@ const memberships = {
     multipurpose: 'Multipurpose Centre',
 }
 
-// const getTableData = tableName => {
-//     return new Promise((resolve, reject) => {
-//         airtable(tableName)
-//             .select()
-//             .firstPage((err, records) => {
-//                 if (err) return reject(err)
-//                 return resolve(
-//                     records.map(record => {
-//                         return {
-//                             id: record.id,
-//                             fields: record.fields,
-//                         }
-//                     })
-//                 )
-//             })
-//     })
-// }
-
 export const getStaticProps = async () => {
     const openDesk = await getTableData(memberships.openDesk)
     const openMemberships = await getTableData(memberships.openMemberships)
